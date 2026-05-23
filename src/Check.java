0bf1192a13315cfabdbb395dd804032391104e40
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-05-22 22:36:39 -0700

    i had a version i liked, but then i ran tests and it didn't account for what if there was only one node, so i was in the process of fixing my code and my time ran out

--- src/Check.java
+++ src/Check.java
@@ -12,6 +12,34 @@ public class Check {
      * @return "even", "odd", or "equal" depending on whether there are more even or odd values
      */
     public static String evenOdd(ListNode head) {
-        return null;
+        if (head == null) {
+            return "equal";
+        }
+        ListNode current = head;
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        if (current.next == null) {
+            if (current)
+        }
+
+        while (current.next != null) {
+            int remainder = current.data % 2;
+            if (remainder > 0) {
+            oddCount++;
+            } else {
+            evenCount++;
+            }
+            current = current.next;
+        }
+
+        if (oddCount > evenCount) {
+            return "odd";
+        }
+        if (evenCount > oddCount) {
+            return "even";
+        }
+        return "equal";
+
     }
 }
